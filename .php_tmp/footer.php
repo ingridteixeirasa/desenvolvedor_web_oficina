d0eba9b876715cf74b9a4699f1fa53544f872d98
Ingrid Teixeira <ingrid_tsalves@hotmail.com>
2022-11-23 20:51:53 -0300

    Aula 23/11

---
+++ .php_tmp/footer.php
@@ -0,0 +1,68 @@
+<div class="container-fluid">
+            <div class="row text-center footer">
+                <div class="col-md-3"> 
+                    <h4>Só Lasanhas</h4>
+                    <p>
+                      Oficina especializada nos piores carros do mercado.
+                    </p>
+                </div>
+                <div class="col-md-3"> 
+                    <h4>Contatos</h4>
+                    
+                      <p>(21)2255-6644</p>
+                      <p>(21)97562-2525</p>
+                      <p>(21)99963-5825</p>
+                    
+                </div>
+                <div class="col-md-3"> 
+                  <h4>Horário de Funcionamento</h4>
+                  
+                  <p>Segunda-feira: 8 às 18h</p>
+                  <p>Terça-feira: 8 às 18h</p>
+                  <p>Quarta-feira: 8 às 18h</p>
+                  <p>Quinta-feira: 8 às 18h</p>
+                  <p>Sexta-feira: 8 às 18h</p>
+                  <p>Sábado: 8 às 18h</p>
+                  
+                </div>
+                <div class="col-md-3"> 
+                  <h4>Busca no Site</h4>
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Mecânica Geral</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Serviços Elétricos</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Nossa Oficina</a>
+                    </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Fale Conosco</a>
+                  </li>
+            </ul>
+                  
+            </div>
+    </div>
+    <div class="row text-center socialmedia">
+      <i class="fa-brands fa-square-facebook"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-youtube"></i>
+
+    </div>
+
+    
+  </div>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEpTy" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
---
+++ .php_tmp/index.php
@@ -0,0 +1,74 @@
+<?php
+     require_once 'head.php';
+     require_once 'menu.php';
+?>
+    <div class="container-fluid">
+        <div class="row principal carrossel">
+            <div class="col-md-3">  </div>
+            <div class="col-md-6"> 
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src="img\scenic.jpg">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="img\c4.jpg">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="img\206.jpg">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="img\marea.jpg">
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Próximo</span>
+                    </a>
+                  </div>
+                  
+            </div>
+
+            <div class="col-md-3">  </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-md-12"> 
+                <h1>Conheça nossos serviços</h1>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
+        <div class="row text-center servicos">
+            <div class="col-md-4"> 
+                <img src="img/oleo.jpg" class="img-fluid">
+                <h3>Troca de Óleo</h3>
+            </div>
+            <div class="col-md-4 servicos"> 
+                <img src="img/alinhamento.webp" class="img-fluid">
+                <h3>Alinhamento</h3>
+            </div>
+            <div class="col-md-4 servicos"> 
+                <img src="img\dh.jpg" class="img-fluid">
+                <h3>Direção Hidrápica</h3>
+            </div>
+
+        </div>
+    </div>
+<?php
+require_once
+?>
+
+   
